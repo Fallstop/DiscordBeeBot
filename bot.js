9d4e2a3d4f2225361f78ade6b0bc713327fc097d
Fallstop <jasper@ngita.co.nz>
2019-10-12 08:27:38 +1300

    RefacterdCode

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 var fs = require('fs');
 var BeeScript = fs.readFileSync('BeeMovieScript.txt', 'utf8');
+var ShrekScript = fs.readFileSync('ShrekScript.txt', 'utf8');
 const delay = require('delay');
 logger.info('Loaded Bee movie script');
 // Configure logger settings
@@ -11,10 +12,6 @@ logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-function SliceMessage(Message) {
-	var SliceMessages = [];
-	
-}
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
@@ -25,39 +22,49 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
+//FUNCTIONS
+
+function SliceMessage(Text) {
+	//Function to Take a Large String and split it into a array
+	var SliceMessages = [];
+	var SliceSection = "";
+	var NumberOfSlices = 0;
+	var i = 0;
+	var x = 2000; //Charcter Limit 
+	NumberOfSlices = Math.ceil(Text/x;
+	while (i < NumberOfSlices - 1){
+		SliceSection = Text.slice(i*x,x*(i+1));
+		SliceMessages.push(SliceSection);
+		i += 1;
+	}
+	SliceSection = Text.slice(i*x,Text.length);
+	SliceMessages.push(SliceSection);
+
+	return(SliceMessages);
+}
+function SendMessages(Array) {
+	var i = 0;
+    while(i < Array.length){
+   		bot.sendMessage({
+        	to: channelID,
+        	message: Array[i]
+    	});
+   		i+= 1;
+   		delay(200);
+    }
+}
+
+//MAIN CODE
+
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (message == 'bee') {
-        var Script = BeeScript;
-        logger.info('Recived Command');
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        var ScriptLength = Script.length;
-        var ScriptSlice = Script.slice(0,2000);
-        var ScriptNumSlice = 1;
-        while (ScriptLength > 2000) {
-            ScriptLength = ScriptLength - 2000;
-            ScriptNumSlice += 1;    
-        }
-        logger.info('Found number of slices neeeded');
-        logger.info(ScriptNumSlice);
-        i = 0
-        while (i < ScriptNumSlice - 1){
-	    var ScriptSlice = Script.slice(i*2000,(i+1)*2000);
-            logger.info("Sending ScriptSlice");
-            bot.sendMessage({
-                to: channelID,
-                message: ScriptSlice
-            });
-            i += 1
-            delay(100);
-        }
-        var ScriptSlice = Script.slice(i*2000,Script.length);
-        bot.sendMessage({
-            to: channelID,
-            message: ScriptSlice
-        });
-        args = args.splice(1);
-     }
+    	ScriptArray = SliceMessage(BeeScript);
+    	SendMessages(ScriptArray);
+    }
+    else if (message == 'shrek'){
+    	ScriptArray = SliceMessage(ShrekScript);
+    	SendMessages(ScriptArray);
+    } 
+
 });
