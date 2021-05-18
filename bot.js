5ea4b1212e718eec455765f0c3a9d7ca8919d6a1
Jasper M-W <jasper@qrl.nz>
2021-05-18 02:40:27 +0000

    Filter out &nbsp; from notice text

--- bot.js
+++ bot.js
@@ -142,14 +142,18 @@ function SendMessages(Array, msg) {
 }
 
 function HtmlToDiscord(input) {
+	//Convert HTML tags to Discord Markdown
 	input = input.replace(/<\/?b>/ig, '**')
+	input = input.replace(/<\/?i>/ig, '*')
 	input = input.replace(/<\/?br>/ig, '\n')
 	input = input.replace(/<\/?em>/ig, '***')
 	input = input.replace(/<\/?strike>/ig, '~~')
 	input = input.replace(/<\/?del>/ig, '~~')
 	input = input.replace(/<\/?s>/ig, '~~')
 	input = input.replace(/<\/?p>/ig, '\n')
+	//General Cleanup of HTML
 	input = input.replace(/<\/?.+>/ig, '')
+	input = input.replace(/&nbsp;/ig, '')
 	return input
 }
 
